29e6e51a44fc81138960ce596824f1c6dcde8b0c
Matt Luker <jamesmattluker@gmail.com>
2022-01-07 16:37:54 -0700

    add AddMachine, AddMachineError and CompletedDetails views and controllers for Engineers

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -61,6 +61,55 @@ namespace Factory.Controllers
       return View(thisEngineer);
     }
 
+    public ActionResult AddMachine(int id)
+    {
+      List<Machine> machines = _db.Machines.ToList();
+      ViewData.Add("machines", machines);
+      var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+      return View(thisEngineer);
+    }
+
+    [HttpPost]
+    public ActionResult AddMachine(Engineer engineer, int MachineId)
+    {
+      bool alreadyExists = _db.EngineerMachine.Any(engineerMachine => engineerMachine.EngineerId == engineer.EngineerId && engineerMachine.MachineId == MachineId);
+      if (MachineId != 0 && !alreadyExists)
+      {
+        _db.EngineerMachine.Add(new EngineerMachine() { MachineId = MachineId, EngineerId = engineer.EngineerId });
+      }
+      _db.SaveChanges();
+      if (alreadyExists)
+      {
+        return RedirectToAction("AddMachineError", new { id = engineer.EngineerId });
+      }
+      return RedirectToAction("Details", new { id = engineer.EngineerId });
+    }
+
+    public ActionResult AddMachineError(int id)
+    {
+      var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
+
+    [HttpPost]
+    public ActionResult SwitchCompleted(int joinId)
+    {
+      var joinEntry = _db.EngineerMachine.FirstOrDefault(entry => entry.EngineerMachineId == joinId);
+      joinEntry.Complete = !joinEntry.Complete;
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = joinEntry.EngineerId });
+    }
+
+    public ActionResult CompletedDetails(int id)
+    {
+      var thisEngineer = _db.Engineers
+        .Include(engineer => engineer.JoinEntities)
+        .ThenInclude(join => join.Machine)
+        .FirstOrDefault(Engineer => Engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
+
     public ActionResult Delete(int id)
     {
       Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);

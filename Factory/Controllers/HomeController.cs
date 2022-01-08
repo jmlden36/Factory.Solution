3560a436b813a84c14d777fcb9164b00afbc54bb
Matt Luker <jamesmattluker@gmail.com>
2022-01-08 15:15:55 -0700

    add all machines and engineers to splash page and edit styling

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
+      List<Engineer> engineers = _db.Engineers.ToList();
+      ViewData.Add("engineers", engineers);
+      List<Machine> machines = _db.Machines.ToList();
+      ViewData.Add("machines", machines);
       return View();
     }
   }
